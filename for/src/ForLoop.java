bd7155b670a65b33f9feb79568ec85f289bdf52a
obidkhon <obid.sharipov.80@mail.ru>
2020-12-18 23:24:13 +0500

    fore

---
+++ for/src/ForLoop.java
@@ -0,0 +1,24 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+
+        for (int i=5;i<=5 ;  i+=4){
+            System.out.println(i+" - olma");
+        }
+
+
+      // int menimgYoshim=40;
+       //increment
+       //++ menimgYoshim;
+       //postincreamnt
+       //menimgYoshim++;
+        /// dicrement
+      //--menimgYoshim;
+        //postdikirement
+        //menimgYoshim--;
+        //menimgYoshim=menimgYoshim+8;
+        //menimgYoshim+=9;
+
+       // System.out.println(menimgYoshim);
+    }
+}
